f86a6d372bbc5214635d1ea5d9470cf0a76ce539
milimk <milimkim@gmail.com>
2024-07-18 00:55:42 -0700

    Basic HTML content with current time JS

---
+++ src/index.js
@@ -0,0 +1,39 @@
+function formatDate(now) {
+  let date = now.getDate();
+  let year = now.getFullYear();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  return `${month} ${date}, ${year}, ${day} ${hours}:${minutes}`;
+}
+
+let currentDateElement = document.querySelector(".current-date");
+let current = new Date();
+currentDateElement.innerHTML = formatDate(current);

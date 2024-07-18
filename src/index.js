9246dcee211abf22337ad9e9cddb9a8ba5a1ca12
milimk <milimkim@gmail.com>
2024-07-18 05:37:43 -0700

    JS Searching and display real time data from API call

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // Current date display
+
 function formatDate(now) {
   let date = now.getDate();
   let year = now.getFullYear();
@@ -47,13 +48,34 @@ let currentDateElement = document.querySelector(".current-date");
 let current = new Date();
 currentDateElement.innerHTML = formatDate(current);
 
-// Search engine
+// Search engine and adding weather API call
+
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city-name");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "0af4f7ebo6ce11605e35ecb7eatc1716";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(updateWeather);
+}
+
 function searchResult(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
+  /* Display search term I used in the input
   let cityElement = document.querySelector("#current-city-name");
   cityElement.innerHTML = searchInput.value;
+  */
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchResult);
+
+searchCity("Vienna");
